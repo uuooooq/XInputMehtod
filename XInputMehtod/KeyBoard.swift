f921f74c0cce5cc80a450fda479c75dbc5a3d2b1
Dongwei.zhu <851450028@qq.com>
2017-07-24 09:19:35 +0800

    给键盘类添加部分功能

--- XInputMehtod/KeyBoard.swift
+++ XInputMehtod/KeyBoard.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class KeyBoard: NSObject {
     
@@ -16,9 +17,22 @@ class KeyBoard: NSObject {
 
     
     init(keyBoardName:NSString, keyBoardDesc:NSString) {
+        
+        func createInitKeys() -> NSArray{
+            //return NSArray();
+            for i in 0...3 {
+                for j in 0...9 {
+                    
+                    var tmpKey = Key.init(isCombine: false, isShow: true, keyDesc: "\(i)\(j)" as NSString, keyInputInfo: "keyinputinfo\(i)\(j)", keyTextColor: UIColor.gray, keyFontSize: 15, keyIndex: );
+                }
+            }
+        }
+        
         self.keyBoardName = keyBoardName
         self.keyBoardDesc = keyBoardDesc
-        self.keys = NSArray()
+        self.keys = createInitKeys()
+        
+
     }
     
 }
